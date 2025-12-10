1a5fd0ceed9c29376195689d569aeafd42800bec
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-10 17:07:38 +0530

    .

--- my_portfolio/src/App.jsx
+++ my_portfolio/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      duration:1000,
+    })
+  }, [])
   return (
-    <div>App</div>
+    <main className='bg-gray-950'>
+
+    </main>
   )
 }
 
