0c68c8ba8d94af2e53fc383a1f900de9752c7941
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-12 11:23:34 +0530

    .

---
+++ Navbar-sidebar/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+
+    const navLinks = [
+        {id:1, name: Home, link: '#home'},
+        {id:2, name: About, link: '#about'},
+        {id:3, name: Skills, link: '#skills'},
+        {id:4, name: projects, link: '#projects'},
+        {id:5, name: Progress, link: '#progress'},
+    ];
+  return (
+    <nav className='flex items-center justify-between'>
+        <a href="#home">Navbar</a>
+        <ul>
+            
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
