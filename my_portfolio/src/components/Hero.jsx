96540656559a6068dd4ca3b88ac39b4b979e07ad
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-12 11:07:56 +0530

    .

--- my_portfolio/src/components/Hero.jsx
+++ my_portfolio/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 const Hero = () => {
   return (
     <div className='relative overflow-hidden min-h-[550px] sm:min-h-[600px] flex flex-col items-center'>
-        <div className='md:h-[550px] h-[500px] w-[450px] bg-gradient-to-r absolute from-[#6d2897] via-[#8e6cf5] to-[#bb61c5] transform rotate-45 z-0 right-2 top-28 rounded-3xl'>
+        <div className='md:h-[550px] h-[500px] w-[450px] bg-linear-to-r absolute from-[#6d2897] via-[#8e6cf5] to-[#bb61c5] transform rotate-45 z-0 right-2 top-28 rounded-3xl'>
         </div>
         <Navbar />
         <main id="home" className='flex flex-col md:flex-row items-center justify-center w-full px-4 md:px-52 pb-4 md:pb-24 md:pt-32 pt-24 mt-10 md:mt-0 z-10'>
