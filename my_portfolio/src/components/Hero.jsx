1c98b3e3ed355d6a2ec66ce055f9f83a1836e2d6
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-12 10:58:39 +0530

    .

--- my_portfolio/src/components/Hero.jsx
+++ my_portfolio/src/components/Hero.jsx
@@ -7,6 +7,9 @@ const Hero = () => {
         <div className='md:h-[550px] h-[500px] w-[450px] bg-gradient-to-r absolute from-[#6d2897] via-[#8e6cf5] to-[#bb61c5] transform rotate-45 z-0 right-2 top-28 rounded-3xl'>
         </div>
         <Navbar />
+        <main id="home" className='flex flex-col md:flex-row items-center justify-center w-full px-4 md:px-52 pb-4 md:pb-24 md:pt-32 pt-24 mt-10 md:mt-0 z-10'>
+          <section className='flex-1 mr-28 md:text-left mt-10 md:mt-0 relative' data-aos='fade-up' data-aos-delay='500'></section>
+        </main>
     </div>
   )
 }
