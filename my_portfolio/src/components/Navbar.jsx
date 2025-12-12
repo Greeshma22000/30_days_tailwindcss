6e99e76988192f4ed90e8ac99c4123e908e49e0f
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-12 10:49:57 +0530

    .

--- my_portfolio/src/components/Navbar.jsx
+++ my_portfolio/src/components/Navbar.jsx
@@ -12,6 +12,7 @@ const Navbar = () => {
   return (
     <header className='fixed top-0 left-0 w-full z-20 text-white' data-aos='fade-up' data-aos-delay='300'>
       <div className='container mx-auto flex items-center justify-between p-5'>
+        {/* logo */}
         <a href="#home" className='text-4xl font-bold italic text-white'>Portfolio{" "}</a>
 
         <button className='md:hidden focus:outline-none' onClick={() => setIsOpen(!isOpen)}>
@@ -28,12 +29,12 @@ const Navbar = () => {
 
       <div className={`${isOpen ? 'block' : 'hidden'} md:hidden bg-[#801b9c] absolute top-0 left-0 w-full h-screen flex flex-col items-center justify-center space-y-8 pt-16`}>
         <button className='absolute top-5 right-5 text-white' onClick={() => setIsOpen(false)}>
-          <FiX classNamew-8 h-8 />
+          <FiX className="w-8 h-8" />
         </button>
 
-        {NavbarLinks.map((link) => {
+        {NavbarLinks.map((link) => (
           <a key={link.id} href={link.link} className='text-lg text-white hover:text-gray-300' onClick={() => setIsOpen(false)}>{link.name}</a>
-        })}
+        ))}
 
         <button className='inline-flex text-white border-2 py-2 px-6 focus:outline-none hover:bg-purple-800 rounded-full text-lg'>Contact</button>
       </div>
