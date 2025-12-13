cd16a4bea5421e018dc5ab9f7abf20889cdce9f7
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-13 10:03:59 +0530

    .

--- Navbar-sidebar/src/App.jsx
+++ Navbar-sidebar/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar2 from './components/Navbar2'
 // import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
       {/* <Navbar /> */}
+      <Navbar2 />
     </div>
   )
 }
