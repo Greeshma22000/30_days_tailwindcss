de2f56fef276ce73cce9d7f6bb5aa36213cbdac0
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-20 15:23:50 +0530

    .

--- Hero_Section/src/components/Hero1.jsx
+++ Hero_Section/src/components/Hero1.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Hero1 = () => {
   return (
     <section className='bg-gray-50 dark:bg-gray-900 text-white'>
-        <div>
-            <div>
-                <h1>Understand User Flow <strong>Increase Conversion</strong></h1>
+        <div className='mx-auto max-w-7xl px-4 py-32 lg:flex lg:h-screen lg:items-center'>
+            <div className='mx-auto max-w-xl text-center'>
+                <h1 className='text-3xl font-extrabold sm:text-5xl'>Understand User Flow <strong className='font-extrabold sm:block text-blue-600'>Increase Conversion</strong></h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis a, ipsa error dicta repellendus animi, sit sunt quod perferendis ad ullam incidunt? Porro illum ad minus, ex velit obcaecati earum.</p>
                 <div>
                     <a href="#">Get Started</a>
