1cb72a7579736fe3a0c8d5230d6a0ef320aea1ee
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-28 15:11:35 +0530

    .

--- Personal_Portfolio/src/layout/Navbar.jsx
+++ Personal_Portfolio/src/layout/Navbar.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const Navbar = () => {
+  
+  const navLinks = [
+    {href: "#about", label: "About"},
+    {href: "#projects", label: "Projects"},
+    {href: "#experience", label: "Experience"},
+    {href: "#testimonials", label: "Testimonials"},
+  ];
   return (
     <header>
         <nav></nav>
